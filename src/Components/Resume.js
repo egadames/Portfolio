8fb29c90ba274b94e9a7f3c0e244e0eeee9b4e8f
Kire Semada <egadames990@gmail.com>
2020-09-18 13:52:30 -0700

    fixed bugs and deleted unused files

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -14,7 +14,7 @@ class Resume extends Component {
   render() {
 
     if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
+      // var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
@@ -27,14 +27,14 @@ class Resume extends Component {
         </div>
       })
 
-      var skills = this.props.data.skills.map((skills)=>{
-        var className = 'bar-expand '+ skills.name.toLowerCase();
-        return (
-          <li key={skills.name}>
-            <span style={{width:skills.level, backgroundColor:this.getRandomColor()}}className={className}></span><em>{skills.name}</em>
-          </li>
-        )
-      })
+      // var skills = this.props.data.skills.map((skills)=>{
+      //   var className = 'bar-expand '+ skills.name.toLowerCase();
+      //   return (
+      //     <li key={skills.name}>
+      //       <span style={{width:skills.level, backgroundColor:this.getRandomColor()}}className={className}></span><em>{skills.name}</em>
+      //     </li>
+      //   )
+      // })
     }
 
     return (
